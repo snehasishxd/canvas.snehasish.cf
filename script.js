d09329c8a10992d412d40f3790b32c8e64bfaecb
Snehasish Roy <105574399+snehasishxd@users.noreply.github.com>
2022-08-31 18:17:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,181 +1,45 @@
-if(window.addEventListener) {
-window.addEventListener('load', function () {
-  var canvas, context, canvaso, contexto;
-  var tool;
-  var tool_default = 'pencil';
+const paintCanvas = document.querySelector( '#canvas_block' );
+const context = paintCanvas.getContext( '2d' );
+context.lineCap = 'round';
 
-  function init () {
-    canvaso = document.getElementById('canvas_block');
-    if (!canvaso) {
-      alert('Error: Canvas not found.');
-      return;
-    }
+const colorPicker = document.querySelector( '#color_selector');
 
-    if (!canvaso.getContext) {
-      alert('Error: No Canvas context.');
-      return;
-    }
+colorPicker.addEventListener( 'change', event => {
+    context.strokeStyle = event.target.value; 
+} );
 
-    contexto = canvaso.getContext('2d');
-    if (!contexto) {
-      alert('Error: Failed to get context.');
-      return;
-    }
+const lineWidthRange = document.querySelector( '.js-line-range' );
+const lineWidthLabel = document.querySelector( '.js-range-value' );
 
-    var container = canvaso.parentNode;
-    canvas = document.createElement('canvas');
-    if (!canvas) {
-      alert('Error: Could not create `temp_canvas`');
-      return;
-    }
-    
-    var ww = window.innerWidth
-    var wh = window.innerHeight
-    canvaso.width = ww - 20
-    canvaso.height = wh - 60
+lineWidthRange.addEventListener( 'input', event => {
+    const width = event.target.value;
+    lineWidthLabel.innerHTML = width;
+    context.lineWidth = width;
+} );
 
-    canvas.id     = 'imageTemp';
-    canvas.width  = canvaso.width;
-    canvas.height = canvaso.height;
-    container.appendChild(canvas);
+let x = 0, y = 0;
+let isMouseDown = false;
 
-    context = canvas.getContext('2d');
-
-    var tool_select = document.getElementById('stroke_style');
-    if (!tool_select) {
-      alert('Error: `stroke_style` Element not found.');
-      return;
-    }
-    tool_select.addEventListener('change', ev_tool_change, false);
-
-    if (tools[tool_default]) {
-      tool = new tools[tool_default]();
-      tool_select.value = tool_default;
-    }
-
-    canvas.addEventListener('mousedown', ev_canvas, false);
-    canvas.addEventListener('mousemove', ev_canvas, false);
-    canvas.addEventListener('mouseup',   ev_canvas, false);
-  }
-
-  function ev_canvas (ev) {
-    if (ev.layerX || ev.layerX == 0) {
-      ev._x = ev.layerX;
-      ev._y = ev.layerY;
-    } else if (ev.offsetX || ev.offsetX == 0) {
-      ev._x = ev.offsetX;
-      ev._y = ev.offsetY;
-    }
-    var func = tool[ev.type];
-    if (func) {
-      func(ev);
-    }
-  }
-  function ev_tool_change (ev) {
-    if (tools[this.value]) {
-      tool = new tools[this.value]();
-    }
-  }
-  function img_update () {
-		contexto.drawImage(canvas, 0, 0);
-		context.clearRect(0, 0, canvas.width, canvas.height);
-  }
-
-  var tools = {};
-
-  tools.pencil = function () {
-    var tool = this;
-    this.started = false;
-
-    this.mousedown = function (ev) {
+const stopDrawing = () => { isMouseDown = false; }
+const startDrawing = event => {
+    isMouseDown = true;   
+   [x, y] = [event.offsetX, event.offsetY];  
+}
+const drawLine = event => {
+    if ( isMouseDown ) {
+        const newX = event.offsetX;
+        const newY = event.offsetY;
         context.beginPath();
-        context.moveTo(ev._x, ev._y);
-        tool.started = true;
-    };
-    this.mousemove = function (ev) {
-      if (tool.started) {
-        context.lineTo(ev._x, ev._y);
+        context.moveTo( x, y );
+        context.lineTo( newX, newY );
         context.stroke();
-      }
-    };
-    this.mouseup = function (ev) {
-      if (tool.started) {
-        tool.mousemove(ev);
-        tool.started = false;
-        img_update();
-      }
-    };
-  };
-  tools.rect = function () {
-    var tool = this;
-    this.started = false;
+        //[x, y] = [newX, newY];
+        x = newX;
+        y = newY;
+    }
+}
 
-    this.mousedown = function (ev) {
-      tool.started = true;
-      tool.x0 = ev._x;
-      tool.y0 = ev._y;
-    };
-
-    this.mousemove = function (ev) {
-      if (!tool.started) {
-        return;
-      }
-
-      var x = Math.min(ev._x,  tool.x0),
-          y = Math.min(ev._y,  tool.y0),
-          w = Math.abs(ev._x - tool.x0),
-          h = Math.abs(ev._y - tool.y0);
-
-      context.clearRect(0, 0, canvas.width, canvas.height);
-
-      if (!w || !h) {
-        return;
-      }
-
-      context.strokeRect(x, y, w, h);
-    };
-
-    this.mouseup = function (ev) {
-      if (tool.started) {
-        tool.mousemove(ev);
-        tool.started = false;
-        img_update();
-      }
-    };
-  };
-  tools.line = function () {
-    var tool = this;
-    this.started = false;
-
-    this.mousedown = function (ev) {
-      tool.started = true;
-      tool.x0 = ev._x;
-      tool.y0 = ev._y;
-    };
-
-    this.mousemove = function (ev) {
-      if (!tool.started) {
-        return;
-      }
-
-      context.clearRect(0, 0, canvas.width, canvas.height);
-
-      context.beginPath();
-      context.moveTo(tool.x0, tool.y0);
-      context.lineTo(ev._x,   ev._y);
-      context.stroke();
-      context.closePath();
-    };
-
-    this.mouseup = function (ev) {
-      if (tool.started) {
-        tool.mousemove(ev);
-        tool.started = false;
-        img_update();
-      }
-    };
-  };
-
-  init();
-
-}, false); }
+paintCanvas.addEventListener( 'mousedown', startDrawing );
+paintCanvas.addEventListener( 'mousemove', drawLine );
+paintCanvas.addEventListener( 'mouseup', stopDrawing );
+paintCanvas.addEventListener( 'mouseout', stopDrawing );
